4e8422cf0029f0f611c13b8f934afe003cd40727
Franco Victorio <victorio.franco@gmail.com>
2017-05-05 04:56:20 +0200

    Add basic usage info (#13)
    * Add basic usage info
    
    * Replace minimist with meow 🐱

--- bin/bin.js
+++ bin/bin.js
@@ -1,13 +1,30 @@
 #!/usr/bin/env node
-const argv = require('minimist')(process.argv.slice(2), {
-  alias: {
-    file: 'f',
-  },
-})
+const meow = require('meow')
+
+const cli = meow(
+  `
+  Usage
+    $ chimi -f file
+
+  Options
+    --file, -f  File or glob matching multiple files
+
+  Examples
+    $ chimi -f README.md
+
+    $ chimi -f doc/*.md
+`,
+  {
+    alias: {
+      f: 'file',
+      h: 'help',
+    },
+  }
+)
 
 const runner = require('../lib/runner')
 const config = require('../lib/config')
 
-const file = argv.file || config.file
+const file = cli.flags.file || config.file
 
 runner(config.timeout, file)
