727d676d86940c6ca51f0930f983cb22faf04f39
gillchristian <gillchristiang@gmail.com>
2017-10-20 08:25:24 +0200

    Update src/ files to match lib/ last changes

--- src/lib.js
+++ src/lib.js
@@ -3,6 +3,7 @@ const fs = require('fs')
 const R = require('ramda')
 const Future = require('fluture')
 const { extract } = require('chipa')
+const { SourceMapGenerator } = require('source-map')
 
 // sanctuary with Fluture types added
 const S = require('./sanctuary')
@@ -29,11 +30,58 @@ const listDependencies = deps =>
     .map(r => r + ';')
     .join('\n')
 
+const generateSourceMaps = (code, dependencies) => {
+  const decoratedCodeFirstLine =
+    1 +
+    1 + // require source-map-suppor
+    1 + // comment
+    dependencies.split('\n').length + // dependencies
+    1 // empty line
+
+  const codeLinesCount = code.split('\n').length
+
+  const map = new SourceMapGenerator({
+    file: '/decorated-snippet.js',
+  })
+
+  for (let i = 0; i < codeLinesCount; i++) {
+    map.addMapping({
+      generated: {
+        line: decoratedCodeFirstLine + i,
+        column: 0,
+      },
+      source: '/snippet.js',
+      original: {
+        line: i + 1,
+        column: 0,
+      },
+    })
+  }
+
+  map.setSourceContent('/snippet.js', code)
+
+  return map.toString()
+}
+
 // injectDependencies :: Object -> String -> String
-const injectDependencies = (deps, code) =>
-  !deps || R.isEmpty(deps)
-    ? code
-    : ['// snippet dependencies', listDependencies(deps), code].join('\n')
+const injectDependencies = (deps, code) => {
+  const dependencies = listDependencies(deps || [])
+  const sourceMaps = generateSourceMaps(code, dependencies)
+  const sourceMapsBase64 = new Buffer(sourceMaps).toString('base64')
+
+  const sourceMapsInline = `//# sourceMappingURL=data:application/json;charset=utf-8;base64,${sourceMapsBase64}`
+
+  const generatedCode = [
+    "require('source-map-support').install()",
+    '// snippet dependencies',
+    dependencies,
+    '',
+    code,
+    sourceMapsInline,
+  ].join('\n')
+
+  return generatedCode
+}
 
 // SnippetData :: { value: String, meta: String, ... }
 // Snippet     :: { value: String, meta: String, id: Int }
